456d6c413bb70c4b0f59d14dac2705d7ca4512ff
Justin Stroup <jstroup@mail.umhb.edu>
2014-10-19 23:47:27 -0500

    update
    …

--- buttsniffer1_0.py
+++ buttsniffer1_0.py
@@ -1,12 +1,26 @@
 from socket import *
+import impacket
+from impacket import ImpactDecoder
 
-serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', serverPort))
+# cl interface
+usrInput = input('argh matie! establish tis protocol: ')
+
+# serverPort = 12000
+protocolNum = getprotobyname(usrInput) # get port num
+serverSocket = socket(AF_INET, SOCK_RAW, protocolNum)
+serverSocket(IPPROTO_IP, IP_HDRINCL, 1)
+# serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 print "The server is ready to recieve"
 
+decoder = ImpactDecoder.IPDecoder()
+
 while 1:
+	# decode and print packet
+	packet = serverSocket.recvfrom(4096)[0]
+	packet = decoder.decode(packet)
+	print packet
+
 	connectionSocket, addr = serverSocket.accept()
 	sentence = connectionSocket.recv(1024)
 	capitalizedSentence = sentence.upper()
--- test.py
+++ test.py
@@ -8,3 +8,9 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # bind s to a public host and port 80
 s.bind((socket.gethostname(), 80))
 s.listen(5)
+
+while 1:
+  # accept connections from outside
+  (clientsocket, address) = s.accept()
+  ct = client_thread(clientsocket)
+  ct.run()
